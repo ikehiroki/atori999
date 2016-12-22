151a0a28e2d680a6316a7d0d17ee4e173fb41c4c
ikemoto <ikemoto@ntts-sv.co.jp>
2016-12-22 16:28:59 +0900

    const

--- server.js
+++ server.js
@@ -4,34 +4,71 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var publisher = require("redis").createClient(6379, 'atoriredis.redis.cache.windows.net');
-publisher.auth('wvSGKLKzvtApqxs4RUdx0ORbzYp5CR8S4NgUXl/kYjE=', function (err) {
-    if (err) {
+const redisHost = 'atoriredis.redis.cache.windows.net';
+const redisPort = 6380;
+const redisKey = 'XFWlUowkF7B7uQCXuwQ9BVPurvgU8NZMqoH5baNLJbA=';
+const redisChannel = 'notify:sid';
+
+const sioRedisHost = 'atoriredis.redis.cache.windows.net';
+const sioRedisPort = 6380;
+const sioRedisKey = 'XFWlUowkF7B7uQCXuwQ9BVPurvgU8NZMqoH5baNLJbA=';
+const sioEventName = 'register';
+
+/**
+ * IDのpub/sub用 publisher
+ */
+var publisher = require('redis').createClient(redisPort, redisHost);
+publisher.auth(redisKey, function (error) {
+    if (error) {
         console.log('auth error!!!');
     }
 });
 
-var subscriber = require("redis").createClient(6379, 'atoriredis.redis.cache.windows.net');
-subscriber.auth('wvSGKLKzvtApqxs4RUdx0ORbzYp5CR8S4NgUXl/kYjE=', function (err) {
-    if (err) {
+/**
+ * IDのpub/sub用 subscriber
+ */
+var subscriber = require('redis').createClient(redisPort, redisHost);
+subscriber.auth(redisKey, function (error) {
+    if (error) {
         console.log('auth error!!!');
     }
 });
 
-var ioredis = require('socket.io-redis');
-io.adapter(ioredis({
-    host: 'atoriredis.redis.cache.windows.net',
-    port: 6379 ,
-    auth_pass: 'wvSGKLKzvtApqxs4RUdx0ORbzYp5CR8S4NgUXl/kYjE='
-}));
+/**
+ * Socket.IO-redis用 publisher
+ */
+var sioPublisher = require('redis').createClient(sioRedisPort, sioRedisHost);
+sioPublisher.auth(sioRedisKey, function (error) {
+    if (error) {
+        console.log('auth error!!!');
+    }
+});
 
+/**
+ * Socket.IO-redis用 publisher
+ */
+var sioSubscriber = require('redis').createClient(sioRedisPort, sioRedisHost);
+sioSubscriber.auth(sioRedisKey, function (error) {
+    if (error) {
+        console.log('auth error!!!');
+    }
+});
+
+/**
+ * Socket.IO-redis
+ */
+var sioRedis = require('socket.io-redis');
+io.adapter(sioRedis({ pubClient: sioPublisher, subClient: sioSubscriber }));
+
+/**
+ * 外部API
+ */
 var https = require('https');
-
 var options = {
     host: '9ohz7mz88h.execute-api.ap-northeast-1.amazonaws.com',
     port: 443,
     path: '/beta',
-//        secureProtocol: 'SSLv3_method',
+    //        secureProtocol: 'SSLv3_method',
     secureProtocol: 'TLSv1_method',
     method: 'POST'
 };
@@ -53,9 +90,12 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('register', function (message) {
+    /**
+     * ID登録
+     */
+    socket.on(sioEventName, function (message) {
         console.log(message + ' registerd.');
-/*        var req = https.request(options, function (res) {
+        var req = https.request(options, function (res) {
             console.log("statusCode: ", res.statusCode);
             console.log("headers: ", res.headers);
 
@@ -68,9 +108,8 @@ io.on('connection', function (socket) {
         req.on('error', function (e) {
             console.error(e);
         });
-*/
         socket.join(message);
-        socket.emit('register', message);
+        socket.emit(sioEventName, message);
     });
 });
 
@@ -80,7 +119,7 @@ io.on('connection', function (socket) {
  */
 app.get('/push', function (request, response) {
     console.log(request.query.sid + ' push is sended.');
-    publisher.publish("notify:sid", request.query.sid);
+    publisher.publish(redisChannel, request.query.sid);
     response.send('push is sended.');
 })
 
@@ -92,10 +131,10 @@ subscriber.on("message", function (channel, message) {
     console.log("sessionID " + message + " was read.");
     var clients = io.sockets.adapter.rooms[message];
     if (clients) {
-        io.to(message).emit('register', 'Notify!!!');
+        io.to(message).emit(sioEventName, 'Notify!!!');
     }
 });
-subscriber.subscribe("notify:sid");
+subscriber.subscribe(redisChannel);
 
 var PORT = process.env.PORT || 3000;
 
